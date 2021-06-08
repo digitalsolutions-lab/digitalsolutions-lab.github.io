15c46be8ce0056349ca494a1b499261456d1e6a4
Flore Laforge <flore.laforge@westpac.com.au>
2021-06-08 17:25:18 +1000

    cop updates

--- COP/domino/global.js
+++ COP/domino/global.js
@@ -10,3 +10,16 @@ $(".main-table__btn-more").click(function () {
 $(".main-table__modal-close").click(function () {
     $(this).parent().hide();
 });
+
+// Expand / Collapse table
+$(".expand-btn__js").click(function () {
+    $(".main-table__history-wrapper").toggleClass("closed").toggleClass("open");
+    $(this).toggle();
+    $(".collapse-btn__js").toggle();
+});
+
+$(".collapse-btn__js").click(function () {
+    $(".main-table__history-wrapper").toggleClass("open").toggleClass("closed");
+    $(this).toggle();
+    $(".expand-btn__js").toggle();
+});
